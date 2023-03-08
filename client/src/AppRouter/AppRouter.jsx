3734783d11f0d8266e59f88f701bec18bbd3571c
Philip Thomsen <philip-th@live.dk>
2023-03-08 12:30:41 +0000

    Update AppRouter.jsx

--- client/src/AppRouter/AppRouter.jsx
+++ client/src/AppRouter/AppRouter.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Routes, { Route } from "react-router-dom"
+import { Route,Routes } from "react-router-dom"
 import App from '../App'
 import Leaderboard from '../Pages/Leaderboard/Leaderboard'
 import SignIn from '../Pages/SignIn/SignIn'
