5f901680096ebae5421c359bba2f36efc64c5547
Philip Thomsen <philip-th@live.dk>
2023-03-08 12:16:25 +0000

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import AppRouter from '../AppRouter/AppRouter'
 import Header from './Components/header/header'
 
 function App() {
   return (
     <div>
       <Header/>
+      <AppRouter />
     </div>
   )
 }
