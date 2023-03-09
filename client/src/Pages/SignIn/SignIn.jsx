6e8e3baa51677f68fd2e3208c057e725db5f7b19
Stefán Snær <82455095+stefansnaerh@users.noreply.github.com>
2023-03-09 11:26:19 +0000

    Login page ready, need to add functionality later

--- client/src/Pages/SignIn/SignIn.jsx
+++ client/src/Pages/SignIn/SignIn.jsx
@@ -5,7 +5,7 @@ import './SignIn.scss'
 function SignIn() {
   return (
     <section className='sign-in-container'>
-      <h1>Sign Up!</h1>
+      <h1>Welcome!</h1>
       <h2>Please enter your details.</h2>
       <form>
         <label htmlFor='username'>Username</label>
@@ -14,6 +14,10 @@ function SignIn() {
         <input name='password' placeholder='Please enter your password'></input>
         <button className='submit-btn'>Sign in!</button>
       </form>
+      <h3>Don’t have a account yet?</h3>
+      <a>
+        <h4>Sign up here!</h4>
+      </a>
     </section>
   )
 }
