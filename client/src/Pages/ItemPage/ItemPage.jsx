59edaca98c6f30744881781cecbe8ccf9a26a69d
Philip Thomsen <philip-th@live.dk>
2023-03-13 09:52:51 +0000

    Ressturant fetch

--- client/src/Pages/ItemPage/ItemPage.jsx
+++ client/src/Pages/ItemPage/ItemPage.jsx
@@ -3,22 +3,37 @@ import './ItemPage.scss'
 
 
 import stockPhoto from '../../images/restaurantStockphoto.jpg'
-
+import CategoryItemsService from '../../Services/categoryitems.service'
+import { useEffect, useState } from 'react'
 
 const ItemPage = ({isHamburgerOpen}) => {
+    const [items ,setItems] = useState(null)
+
+    useEffect(() => {
+   CategoryItemsService.getAll().then((response) => {
+    setItems(response.data);
+    
+  });
+  
+    }, [])
+    console.log(items);
+    
     return (
         
         <section className='item-page-container'>
             { !isHamburgerOpen ? (<>
                 <h1>Restaurants</h1>
-                <div className='text-image-container'>
+                {items ? items.map((item) =>
+    <div className='text-image-container'>
                     <img src={stockPhoto}/>
                     <div>
                         <span className='item-name-container'>
-                            <h2>Name</h2>
+                            <h2>{item.name}</h2>
                         </span>
                     </div>
                 </div>
+) : null}
+                
             </>): null}
         </section>
     )
