66c62419d226b22f913949f6657d2c7c80c6d52b
Philip Thomsen <philip-th@live.dk>
2023-03-13 18:07:25 +0000

    Update

--- client/src/Pages/Newsfeed/Newsfeed.jsx
+++ client/src/Pages/Newsfeed/Newsfeed.jsx
@@ -1,21 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import CategoryItemsService from '../../Services/categoryitems.service';
+import PostService from '../../Services/post.service';
+import UserService from '../../Services/user.service';
 import "./Newsfeed.scss"
 function Newsfeed() {
+  let postData;
+  const [items, setItems] = useState({
+    posts: "",
+    userData: ""
+  });
+    const getEverything = () => {
+        PostService.getAll().then((response) => {
+            
+            postData = response.data
+            console.log(items);
+
+        }).catch((e) => {
+            console.log(e)
+        });
+         
+           UserService.getAll().then((response) => {
+          
+             setItems({...items, posts: postData, userData: response.data})
+         })
+         
+    }
+
+    useEffect(() => {
+      
+        getEverything();
+      console.log(items); 
+      for (let index = 0; index < items.userData.length; index++) {
+        const element = items.userData.id[index];
+        console.log(element.indexOf(1));
+        
+      }
+    }, []);
+    
   return (
     <section className='newsfeed-container'>
        <h1>Newsfeed</h1>
+       {items.posts ? (<>
+                
+                {items.posts.map((post, index) =>
+                <article key={index}>
+                <div>
+                  <img src={post.image} alt="profile" />
+                  <div>
+                  <h2> just got 120 points for picking up trash</h2> 
+                  <p>@{!post.location ? "Unknown" : post.location}</p> 
+                  </div>
+                  <img src="" alt="post image" />
+                  <p>{!post.likes ? 0 : post.likes} Likes</p>
+                </div>
+            </article>
+                 ) }
+            </>) : <h2>No posts yet??? </h2>}
 
-<article>
-    <div>
-      <img src="" alt="profile" />
-      <div>
-      <h2>You just got 120 points for picking up trash</h2> 
-      <p>@ Las Palmas Beach</p> 
-      </div>
-      <img src="" alt="post image" />
-      <p>3 likes </p>
-    </div>
-</article>
     </section>
   )
 }
