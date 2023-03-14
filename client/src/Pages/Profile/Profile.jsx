af945e598dc9a75e02561bb0f57d9295d0b1f68f
Philip Thomsen <philip-th@live.dk>
2023-03-14 21:45:24 +0000

    update

--- client/src/Pages/Profile/Profile.jsx
+++ client/src/Pages/Profile/Profile.jsx
@@ -1,10 +1,23 @@
-
-
-
-
+import "./Profile.scss"
+import postImage from '../../images/restaurantStockphoto.jpg'
+import profileImage from '../../images/pexels-photo-220453.png'
 const Profile = ( ) => {
     return (
-        <div> Profile </div>
+        <section className="profile-container">
+            <article>
+                <img src={profileImage} alt="profile picture" />
+                <div>
+                    <p>Points: 200</p>
+                    <p>Lorem Ipsum</p>
+                </div>
+            </article>
+            <article>
+<img src={postImage} alt="post" />
+<img src={postImage} alt="post" />
+<img src={postImage} alt="post" />
+<img src={postImage} alt="post" />
+            </article>
+        </section>
     )
 }
  
