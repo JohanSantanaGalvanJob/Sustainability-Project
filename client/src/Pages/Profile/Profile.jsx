ddd2b9411c26b67d8bef04ab002e817d8ad5c983
unknown <johansantanagalvan.job@gmail.com>
2023-03-15 22:57:11 +0000

    done fetching user in user profile

--- client/src/Pages/Profile/Profile.jsx
+++ client/src/Pages/Profile/Profile.jsx
@@ -9,74 +9,74 @@ import { useEffect, useState } from "react"
 
 
 const Profile = () => {
-    let userDatas
-    let userPosts = []
-    const [items, setItems] = useState({
-        posts: "",
-        userData: ""
-        
-      });
-        const getProfile = () => {
-            UserService.get(sessionStorage.getItem('userId')).then((response) => {
-                
-              userDatas = response.data 
-                console.log(items);
-    
-            }).catch((e) => {
-                console.log(e)
-            });
-            PostService.getAll().then((response) => {
-                
-                setItems({...items, posts: response.data, userData: userDatas})
-                  console.log(items);
-      
-              }).catch((e) => {
-                  console.log(e)
-              });
-             
-        }
-    
-        useEffect(() => {
-          
-            getProfile();
-           
-    
-        }, []);
-try {
-    for (let index = 0; index < items.posts.length; index++) {
-        const element = items.posts[index];
-        if (element.userId == sessionStorage.getItem('userId')) {
-            userPosts.push(element)
-            console.log(element);
-        }
-        
-    }
-} catch (error) {
-    
-}
-    return (
-        <section className="profile-container">
-            <article>
-                <img src={profileImage} alt="profile picture" className="profilePicture" />
-                <div className="profileInfo">
-                    <p className="profileBio">Lorem Ipsum</p>
-                    <p className="profileDetails">{items.userData.points} points</p>
-                    <img src={leafProfileBio} alt="Leaf containing profile-bio" className="profileBioImg" />
-                    <img src={leafProfileDetails} alt="Leaf containing profile-details" className="profileDetailsImg" />
-                </div>
-            </article>
-            <article className="profilePosts">
-{userPosts ? (<>
-                
-                {userPosts.map((post, index) =>
-                
-                    
-                    <img src={post.image} alt="profile" />
 
-                 ) }
-            </>) : <h2> No posts yet </h2>}
-            </article>
-        </section>
+    let userId = sessionStorage.getItem('userId');
+    const [user, setUser] = useState([]);
+    const urlImage = 'http://localhost:8080/public/images/'
+
+    const getProfile = () => {
+        UserService.get(userId).then((response) => {
+            setUser(response.data);
+            console.log(response.data);
+        }).catch((e) => {
+            console.log(e)
+        });
+        // PostService.getAll().then((response) => {
+        //     setItems({ ...items, posts: response.data, userData: userDatas })
+        //     console.log(items);
+        // }).catch((e) => {
+        //     console.log(e)
+        // });
+
+    }
+
+    useEffect(() => {
+        getProfile();
+    }, []);
+    // try {
+    //     for (let index = 0; index < items.posts.length; index++) {
+    //         const element = items.posts[index];
+    //         if (element.userId == sessionStorage.getItem('userId')) {
+    //             userPosts.push(element)
+    //             console.log(element);
+    //         }
+
+    //     }
+    // } catch (error) {
+
+    // }
+    return (
+        <>
+        {/* {console.log(user)} */}
+            {!!user ?
+           
+                <section className="profile-container">
+                   
+                    <article>
+                        <img src={urlImage + user.image} alt="profile picture" className="profilePicture" />
+                        <div className="profileInfo">
+                            <p className="profileBio">{user.username}</p>
+                            <p className="profileDetails">{user.points} points</p>
+                            <img src={leafProfileBio} alt="Leaf containing profile-bio" className="profileBioImg" />
+                            <img src={leafProfileDetails} alt="Leaf containing profile-details" className="profileDetailsImg" />
+                        </div>
+                    </article>
+                    <article className="profilePosts">
+                        {/* {userPosts ? (<>
+
+            {userPosts.map((post, index) =>
+
+
+                <img src={post.image} alt="profile" />
+
+            )}
+        </>) : <h2> No posts yet </h2>} */}
+                    </article>
+                </section>
+
+                : null}
+
+        </>
     )
 }
 
